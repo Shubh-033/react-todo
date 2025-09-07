8a7facd8152b311297421cdc31b8710fb8a5df49
Subham Singh Negi <subhamsinghnegi03@gmail.com>
2025-09-07 10:26:57 +0530

    Fix build configuration for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,54 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+      publicPath: '/',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        minify: isProduction,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 3000,
-    hot: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      port: 3000,
+      hot: true,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    mode: isProduction ? 'production' : 'development',
+    optimization: isProduction ? {
+      splitChunks: {
+        chunks: 'all',
+      },
+    } : {},
+  };
 };
